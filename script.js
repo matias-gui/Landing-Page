d4f75130c456ccab7df9f2306a41ff0af9367c3b
Guilherme Matias <guimatias890@gmail.com>
2025-08-25 22:33:33 -0300

    responsividade

--- script.js
+++ script.js
@@ -29,32 +29,50 @@ const gitlink = document.getElementById('github');
     // Mostrar a primeira imagem inicialmente
     mostrarimg(img);
 
-     projetolink.href = 'https://github.com/matias-gui/projeto-login';
+     projetolink.href ='https://matias-gui.github.io/projeto-login/';
      projetolink.target = '_blank';
-     gitlink.href = 'https://matias-gui.github.io/projeto-login';
+     gitlink.href = 'https://github.com/matias-gui/projeto-login';
      gitlink.target = '_blank';
 
     function atualizarLinks(imgn) {
         
      if(imgn === 0){
-            projetolink.href = 'https://github.com/matias-gui/projeto-login';
+            projetolink.href = 'https://matias-gui.github.io/projeto-login';
             projetolink.target = '_blank';
-            gitlink.href = 'https://matias-gui.github.io/projeto-login';
+            gitlink.href = 'https://github.com/matias-gui/projeto-login';
             gitlink
     }else if(imgn === 1){
-            projetolink.href = 'https://github.com/matias-gui/site-android';
+            projetolink.href = 'https://matias-gui.github.io/site-android/';
             projetolink.target = '_blank';
-            gitlink.href = 'https://matias-gui.github.io/site-android/';
+            gitlink.href = 'https://github.com/matias-gui/site-android';
             gitlink.target = '_blank';
         } else if(imgn === 2){
-            projetolink.href = 'https://github.com/matias-gui/projeto-cordel-moderno';
+            projetolink.href = 'https://matias-gui.github.io/projeto-cordel-moderno/';
             projetolink.target = '_blank';
-            gitlink.href = 'https://matias-gui.github.io/projeto-cordel-moderno/';
+            gitlink.href = 'https://github.com/matias-gui/projeto-cordel-moderno';
             gitlink.target = '_blank';
         }else if(imgn === 3){
-            projetolink.href = 'https://github.com/matias-gui/projeto-redes-sociais';
+            projetolink.href = 'https://matias-gui.github.io/projeto-redes-sociais/';
             projetolink.target = '_blank';
-            gitlink.href = 'https://matias-gui.github.io/projeto-redes-sociais/';
+            gitlink.href = 'https://github.com/matias-gui/projeto-redes-sociais';
             gitlink.target = '_blank';
         }
     }
+    const msg = "Entre em Contato";
+    const digitarmsg = document.getElementById('mensagem');
+    let i = 0;
+
+    function digitar(){
+        // Verifica se ainda há caracteres para digitar
+        if(i < msg.length){
+            // Adiciona o próximo caractere à mensagem
+            digitarmsg.textContent += msg.charAt(i);
+            i++;
+            // Chama a função novamente após um atraso
+            setTimeout(digitar, 200);
+
+        }
+    }
+
+    digitar()
+
